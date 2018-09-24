8f53c139ac10259d6d6e68df3b4696c0c34b7418
Aaron <aaronmassey45@gmail.com>
2018-09-24 11:53:58 -0500

    Moved 'done()' inside on end function to stop all tests from passing. Fixed tests with issues

--- server/tests/functional.tests.js
+++ server/tests/functional.tests.js
@@ -14,9 +14,9 @@ const { books, populateBooks } = require('./seed-data');
 
 chai.use(chaiHttp);
 
-beforeEach(populateBooks);
-
 describe('Functional Tests', function() {
+  this.timeout(10000);
+  beforeEach(populateBooks);
   describe('example GET /api/books', function() {
     /*
     * ----[EXAMPLE TEST]----
@@ -78,8 +78,8 @@ describe('Functional Tests', function() {
               title,
               'The book title should equal the input'
             );
+            done();
           });
-        done();
       });
 
       it('should not add new book without providing title', function(done) {
@@ -91,8 +91,8 @@ describe('Functional Tests', function() {
             assert.equal(res.status, 400);
             assert.property(res.body, 'error', 'This should cause an error');
             assert.equal(res.body.error, 'no title was provided');
+            done();
           });
-        done();
       });
     });
 
@@ -134,8 +134,8 @@ describe('Functional Tests', function() {
             assert.isObject(res.body, 'response should be an object');
             assert.property(res.body, 'error', 'This should cause an error');
             assert.equal(res.body.error, 'invalid id');
+            done();
           });
-        done();
       });
 
       it('should return book if provided with valid id in db', function(done) {
@@ -153,17 +153,46 @@ describe('Functional Tests', function() {
               'Book should contain an array of comments'
             );
             assert.isArray(res.body.comments, 'comments should be an array');
-            assert.equal(res.body._id, book[0]._id);
-            assert.equal(res.body.title, book[0].title);
-            assert.equal(res.body.comments, book[0].comments);
+            assert.equal(res.body._id, books[0]._id, 'ids are not equal');
+            assert.equal(
+              res.body.title,
+              books[0].title,
+              'titles are not equal'
+            );
+            assert.deepEqual(
+              res.body.comments,
+              books[0].comments,
+              'arrays do not have same comments'
+            );
+            done();
           });
-        done();
       });
     });
 
-    // describe('POST /api/books/[id] => add comment/expect book object with id', function() {
-    //   it('Test POST /api/books/[id] with comment', function(done) {
-    //     //done();
+    // describe('POST /api/books/:id', function() {
+    //   it('should add comment and return book object', function(done) {
+    //     const comment = 'Totally tubular';
+    //     chai
+    //       .request(server)
+    //       .post('/api/books/' + books[1]._id)
+    //       .send({ comment })
+    //       .end(function(err, res) {
+    //         console.log(res.body);
+    //         assert.equal(res.status, 200);
+    //         assert.isObject(res.body, 'response should be an object');
+    //         assert.property(res.body, 'title', 'Book should contain title');
+    //         assert.property(res.body, '_id', 'Book should contain _id');
+    //         assert.property(
+    //           res.body,
+    //           'comments',
+    //           'Book should contain an array of comments'
+    //         );
+    //         assert.isArray(res.body.comments, 'comments should be an array');
+    //         assert.equal(res.body._id, books[1]._id);
+    //         assert.equal(res.body.title, books[1].title);
+    //         // assert.equal(res.body.comments[-1], comment);
+    //       });
+    //     done();
     //   });
     // });
   });
