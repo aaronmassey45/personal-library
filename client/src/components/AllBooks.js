575c7213f5c712876f7e967ff6a6d7b6e8d4c83e
Aaron <aaronmassey45@gmail.com>
2018-09-25 12:32:39 -0500

    Added function to select a book

--- client/src/components/AllBooks.js
+++ client/src/components/AllBooks.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import axios from 'axios';
 import AddNewBook from './AddNewBook';
 
-const AllBooks = ({ values: { books } }) => (
-  <ul className="collection with-header">
-    <li className="collection-header">
-      <AddNewBook />
-    </li>
-    {books.map(book => (
-      <li key={book._id} className="collection-item">
-        {book.title}
-        <span className="right">{book.commentcount} comments</span>
-      </li>
-    ))}
-  </ul>
-);
+const AllBooks = ({ values: { books, dispatch } }) => {
+  const selectBook = async id => {
+    const res = await axios.get(`/api/books/${id}`);
+    dispatch({ type: 'SELECT_BOOK', payload: res.data });
+  };
+
+  return (
+    <div className="col s12 m6">
+      <ul className="collection with-header">
+        <li className="collection-header">
+          <AddNewBook />
+        </li>
+        {books.map(book => (
+          <li
+            key={book._id}
+            className="collection-item"
+            onClick={() => selectBook(book._id)}
+          >
+            {book.title}
+            <span className="right">{book.commentcount} comments</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default AllBooks;
