1e889dc765ae735d8d9e46f3aee5921d06f0eb6b
Aaron <aaronmassey45@gmail.com>
2018-09-25 14:53:55 -0500

    Display add comment form

--- client/src/components/SelectedBook.js
+++ client/src/components/SelectedBook.js
@@ -1,32 +1,33 @@
 import React from 'react';
 
-export default ({ selectedBook }) => {
-  return (
-    <div className="col s12 m6">
-      <div className="card blue-grey darken-1">
-        <div className="card-content white-text">
-          {!selectedBook ? (
-            'No Book Selected'
-          ) : (
-            <div>
-              <span className="card-title">
-                <h4>{selectedBook.title}</h4>
-              </span>
-              <div className="divider" />
-              <h5>Comments</h5>
-              {selectedBook.comments.length > 0 ? (
-                <ul>
-                  {selectedBook.comments.map((comment, i) => (
-                    <li key={`comment-${i}`}>- {comment}</li>
-                  ))}
-                </ul>
-              ) : (
-                'No Comments'
-              )}
-            </div>
-          )}
-        </div>
+import AddComment from './AddComment';
+
+export default ({ selectedBook, dispatch }) => (
+  <div className="col s12 m6">
+    <div className="card blue-grey darken-1">
+      <div className="card-content white-text">
+        {!selectedBook ? (
+          'No Book Selected'
+        ) : (
+          <div>
+            <span className="card-title">
+              <h4>{selectedBook.title}</h4>
+            </span>
+            <div className="divider" />
+            <h5>Comments</h5>
+            {selectedBook.comments.length > 0 ? (
+              <ul>
+                {selectedBook.comments.map((comment, i) => (
+                  <li key={`comment-${i}`}>- {comment}</li>
+                ))}
+              </ul>
+            ) : (
+              'No Comments'
+            )}
+          </div>
+        )}
       </div>
+      {selectedBook && <AddComment id={selectedBook._id} />}
     </div>
-  );
-};
+  </div>
+);
