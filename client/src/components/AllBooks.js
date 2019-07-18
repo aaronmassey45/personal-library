c2b32a0e2eb230d28662fa7243861cf8e27655c8
Aaron <aaronmassey45@gmail.com>
2019-07-17 22:49:29 -0500

    Refactored to work with new books prop and Book component

--- client/src/components/AllBooks.js
+++ client/src/components/AllBooks.js
@@ -2,30 +2,20 @@ import React from 'react';
 
 import AddNewBook from './AddNewBook';
 import DeleteAllButton from './DeleteAllButton';
+import Book from './Book';
 
-import { selectBook } from '../actions';
-
-const AllBooks = ({ values: { books, dispatch } }) => {
-  return (
-    <div className="col s12 m6">
-      <ul className="collection with-header">
-        <li className="collection-header">
-          <DeleteAllButton />
-          <AddNewBook />
-        </li>
-        {Object.values(books).map(book => (
-          <li
-            key={book._id}
-            className="collection-item"
-            onClick={() => dispatch(selectBook(book._id))}
-          >
-            {book.title}
-            <span className="right">{book.commentcount} comments</span>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+const AllBooks = ({ books }) => (
+  <div className="col s12 m6">
+    <ul className="collection with-header">
+      <li className="collection-header">
+        <DeleteAllButton />
+        <AddNewBook />
+      </li>
+      {Object.values(books).map(book => (
+        <Book key={book._id} {...book} />
+      ))}
+    </ul>
+  </div>
+);
 
 export default AllBooks;
