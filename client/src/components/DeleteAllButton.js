1db9051eb50885aea8b6dcf5f893da2d2efcab02
Aaron <aaronmassey45@gmail.com>
2019-07-17 19:10:07 -0500

    Properly disptach action

--- client/src/components/DeleteAllButton.js
+++ client/src/components/DeleteAllButton.js
@@ -3,18 +3,16 @@ import mapDispatchToProps from '../HOC/mapDispatchToProps';
 
 import { deleteAllBooks } from '../actions';
 
-const DeleteAllButton = ({ dispatch }) => {
-  return (
-    <div className="col s12 mb-2">
-      <button
-        className="waves-effect waves-light btn-large red accent-4"
-        onClick={() => deleteAllBooks(dispatch)}
-      >
-        <i className="material-icons right">warning</i>
-        DELETE ALL BOOKS
-      </button>
-    </div>
-  );
-};
+const DeleteAllButton = ({ dispatch }) => (
+  <div className="col s12 mb-2">
+    <button
+      className="waves-effect waves-light btn-large red accent-4"
+      onClick={() => dispatch(deleteAllBooks())}
+    >
+      <i className="material-icons right">warning</i>
+      DELETE ALL BOOKS
+    </button>
+  </div>
+);
 
 export default mapDispatchToProps(DeleteAllButton);
